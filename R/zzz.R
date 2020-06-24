a3de6a0f7e400112e20b94ee76fb4e253cc04181
Jakob Russel <cmf483@science.domain>
2020-06-24 16:19:51 +0200

    v2.7.16 - Fix problem with parallel on macOS in R4.0 #13. Man pages rebuild with new roxygen

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,7 @@
 .onAttach <- function(libname, pkgname){
-  packageStartupMessage("DAtest version 2.7.15")
+  packageStartupMessage("DAtest version 2.7.16")
+  if (Sys.getenv("RSTUDIO") == "1" && !nzchar(Sys.getenv("RSTUDIO_TERM")) && 
+    Sys.info()["sysname"] == "Darwin" && getRversion() == "4.0.0") {
+    snow::setDefaultClusterOptions(setup_strategy = "sequential")
+  }
 }
